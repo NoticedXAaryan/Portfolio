1a75e64ee7c3a811acf5576119f272ae90acce12
Aaryan Kumar Tiwari <157978892+NoticedXAaryan@users.noreply.github.com>
2024-12-09 01:58:30 +0530

    A fresh start.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import floating from "@/components/ui"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <floating/>
+  )
+}
+
+export default App
