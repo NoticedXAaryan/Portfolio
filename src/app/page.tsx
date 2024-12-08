0ad510558cebc24e398339d468590863f0dae35b
Aaryan Kumar Tiwari <157978892+NoticedXAaryan@users.noreply.github.com>
2024-12-08 22:11:55 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import floatingdock from '@/src/components/ui';
+import timeline from '@/src/components/ui';
+
+export const metadata = {
+  title: "Portfolio of Aaryan Kumar Tiwari",
+  description: "NoticedXAaryan",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Display the Floating Dock */}
+        <floatingdock/>
+
+        <timeline/>
+
+        {/* Main content */}
+        {children}
+      </body>
+    </html>
+  );
 }
+
